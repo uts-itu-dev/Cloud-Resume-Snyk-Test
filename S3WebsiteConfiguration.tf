9d882c60aa536a921320713586573133bf1199e4
WichaelMu <w.michael.u@gmail.com>
2023-08-22 14:03:46 +1000

    Add MIME type to index.html and error.html

--- S3WebsiteConfiguration.tf
+++ S3WebsiteConfiguration.tf
@@ -13,15 +13,17 @@ resource "aws_s3_bucket_website_configuration" "m" {
 }
 
 resource "aws_s3_object" "index" {
-  bucket = aws_s3_bucket.b.id
-  key    = "index.html"
-  source = "Source/index.html"
-  etag   = filemd5("Source/index.html")
+  bucket       = aws_s3_bucket.b.id
+  key          = "index.html"
+  source       = "Source/index.html"
+  etag         = filemd5("Source/index.html")
+  content_type = "text/html"
 }
 
 resource "aws_s3_object" "error" {
-  bucket = aws_s3_bucket.b.id
-  key    = "error.html"
-  source = "Source/error.html"
-  etag   = filemd5("Source/error.html")
+  bucket       = aws_s3_bucket.b.id
+  key          = "error.html"
+  source       = "Source/error.html"
+  etag         = filemd5("Source/error.html")
+  dontent_type = "text/html"
 }
