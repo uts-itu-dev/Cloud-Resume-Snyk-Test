5db0c7118883c26d6eb1e66ac227133da49c3e60
WichaelMu <w.michael.u@gmail.com>
2023-09-01 16:14:12 +1000

    CNAME in CloudFront

--- CloudFront.tf
+++ CloudFront.tf
@@ -13,6 +13,11 @@ resource "aws_cloudfront_distribution" "m" {
   is_ipv6_enabled     = true
   default_root_object = "index.html"
 
+  aliases = [
+    "${var.R53DomainName}",
+    "www.${var.R53DomainName}",
+  ]
+
   default_cache_behavior {
     # CachingDisabled managed policy.
     cache_policy_id        = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad"
