c94eb8e110c9728ed8d30778390ff2f74a1a8888
WichaelMu <w.michael.u@gmail.com>
2023-09-01 11:35:20 +1000

    Create R53 Records for Certificate

--- Certificate.tf
+++ Certificate.tf
@@ -24,3 +24,25 @@ resource "aws_acm_certificate" "m" {
     Misc = "Made with Terraform."
   }
 }
+
+resource "aws_route53_record" "m" {
+  for_each = {
+    for dvo in aws_acm_certificate.m.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_zone.m.zone_id
+
+  tags = {
+    Name = "R53 for ${var.R53DomainName}."
+    Misc = "Made with Terraform."
+  }
+}
--- variables.tf
+++ variables.tf
@@ -3,4 +3,8 @@
 variable "R53DomainName" {
   default = "wichaelmu.com"
 }
+
+variable "R53ZoneID" {
+  default = "Z01267694NTYQD3E67XI"
+}
 ################################################################################
