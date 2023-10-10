d0b704aa25696f1008ac8ddc6ec2e5f06f4f4acf
WichaelMu <w.michael.u@gmail.com>
2023-10-11 10:57:00 +1100

    Add More Defined AssumeRole

--- AssumeRole.tf
+++ AssumeRole.tf
@@ -1,32 +1,40 @@
 
 
-resource "aws_iam_policy" "snyk" {
-  name   = "Policy-Snyk"
-  path   = "/"
-  policy = <<__TERMINATE__
-{
-"Version": "2012-10-17",
-"Statement": [
-        {
-        "Effect": "Allow",
-        "Principal": "*",
-        "Action": [
-                "sts:AssumeRole"
-        ],
-        "Resource": [
-                "*"
-        ]
-        }
-]
-}
-    __TERMINATE__
+provider "aws" {
+  alias   = "Source"
+  profile = "Source"
+  region  = "ap-southeast-2"
 }
 
-resource "aws_iam_policy" "snyk-2" {
-  name   = "Policy-Snyk"
-  path   = "/"
-  policy = jsonencode({
-        actions = ["sts:AssumeRole"]
-        resources = ["*"]
-  })
+provider "aws" {
+  alias   = "Destination"
+  profile = "Destination"
+  region  = "ap-southeast-2"
+}
+
+data "aws_caller_identity" "Source" {
+  provider = aws.Source
+}
+
+data "aws_iam_policy_document" "AR" {
+  provider = aws.Destination
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.Source.account_id}:root"]
+    }
+  }
+}
+
+data "aws_iam_policy" "EC2" {
+  provider = aws.Destination
+  name     = "AmazonEC2FullAccess"
+}
+
+resource "aws_iam_role" "AR" {
+  provider            = aws.Destination
+  name                = "AR"
+  assume_role_policy  = data.aws_iam_policy_document.AR.json
+  managed_policy_arns = [data.aws_iam_policy.EC2.arn]
 }
