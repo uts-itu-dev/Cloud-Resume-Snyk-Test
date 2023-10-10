1d0f27d322485217307503c0e6ac8eedd9926eb8
WichaelMu <w.michael.u@gmail.com>
2023-10-11 10:34:59 +1100

    Add Inline Policy

--- AssumeRole.tf
+++ AssumeRole.tf
@@ -1,19 +1,23 @@
-data "aws_iam_policy_document" "snyk" {
-  statement {
-    sid = "1"
 
-    actions = [
-      "sts:AssumeRole",
-    ]
-
-    resources = [
-      "*",
-    ]
-  }
-}
 
 resource "aws_iam_policy" "snyk" {
   name   = "Policy-Snyk"
   path   = "/"
-  policy = data.aws_iam_policy_document.snyk.json
+  policy = <<__TERMINATE__
+{
+"Version": "2012-10-17",
+"Statement": [
+        {
+        "Effect": "Allow",
+        "Principal": "*",
+        "Action": [
+                "sts:AssumeRole"
+        ],
+        "Resource": [
+                "*"
+        ]
+        }
+]
+}
+    __TERMINATE__
 }
