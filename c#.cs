2eeffb67a50d682ab16eae65085269a665849fc2
WichaelMu <w.michael.u@gmail.com>
2023-11-09 10:40:11 +1100

    C# PR TEST 2

--- c#.cs
+++ c#.cs
@@ -17,7 +17,7 @@ class SQLInjection {
                 SqlCredential SC = new SqlCredential("Hardcoded Username", SS);
 
                 string queryString =
-                        $"SELECT CustomerID FROM dbo.Orders WHERE username='{Username}';";
+                        $"SELECT CustomerID FROM dbo.Orders WHERE username='{Username}' AND password='{Args[0]}';"; // LOLOLOLOLO
                 using (SqlConnection connection = new SqlConnection(
                         connectionString, SC))
                 {
